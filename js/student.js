6924f38e0b8b554d229bea9ee392915cad59b617
amald <amald@qburst.com>
2019-07-31 19:01:36 +0530

    Add View Search Student Employee

--- js/student.js
+++ js/student.js
@@ -7,6 +7,31 @@ function selectChange(orga){
 		document.getElementById("slideshow").innerHTML = "<img src='images/employee.jpg' class='banner mySlides w3-animate-fading' alt='banner'></div>";
 	}
 }
+function search() {
+	var org=getOrg();
+  var input, filter, table, tr, td1, td2, td3, td4, i, txtValue1, txtValue2, txtValue3, txtValue4;
+  input = document.getElementById(org+"_input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById(org+"_atable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td1 = tr[i].getElementsByTagName("td")[0];
+    td2 = tr[i].getElementsByTagName("td")[1];
+    td3 = tr[i].getElementsByTagName("td")[2];
+    td4 = tr[i].getElementsByTagName("td")[3];
+    if (td1) {
+      txtValue1 = td1.textContent || td1.innerText;
+      txtValue2 = td2.textContent || td2.innerText;
+      txtValue3 = td3.textContent || td3.innerText;
+      txtValue4 = td4.textContent || td4.innerText;
+      if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1  || txtValue3.toUpperCase().indexOf(filter) > -1  || txtValue4.toUpperCase().indexOf(filter) > -1 ) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 function addRecord(obj,org){
 	var totalObj;
 	var count=localStorage.getItem(org+"_count");
