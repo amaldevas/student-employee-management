de9e6ab426c27bd0c601f693d81393944f6d540b
amald <amald@qburst.com>
2019-08-06 15:09:03 +0530

    Implemented Search Sort Operation

--- js/student.js
+++ js/student.js
@@ -8,86 +8,86 @@ function selectChange(orga){
 	}
 }
 function search() {
-	var org=getOrg();
-  var input, filter, table, tr, td1, td2, td3, td4, i, txtValue1, txtValue2, txtValue3, txtValue4;
-  input = document.getElementById(org+"_input");
-  filter = input.value.toUpperCase();
-  table = document.getElementById(org+"_atable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td1 = tr[i].getElementsByTagName("td")[0];
-    td2 = tr[i].getElementsByTagName("td")[1];
-    td3 = tr[i].getElementsByTagName("td")[2];
-    td4 = tr[i].getElementsByTagName("td")[3];
-    if (td1) {
-      txtValue1 = td1.textContent || td1.innerText;
-      txtValue2 = td2.textContent || td2.innerText;
-      txtValue3 = td3.textContent || td3.innerText;
-      txtValue4 = td4.textContent || td4.innerText;
-      if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1  || txtValue3.toUpperCase().indexOf(filter) > -1  || txtValue4.toUpperCase().indexOf(filter) > -1 ) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
+	let org=getOrg();
+  	let input, filter, table, tr, td1, td2, td3, td4, i, txtValue1, txtValue2, txtValue3, txtValue4;
+	  input = document.getElementById(`${org}_input`);
+	  filter = input.value.toUpperCase();
+	  table = document.getElementById(`${org}_atable`);
+	  tr = table.getElementsByTagName("tr");
+	  for (i = 0; i < tr.length; i++) {
+	    td1 = tr[i].getElementsByTagName("td")[0];
+	    td2 = tr[i].getElementsByTagName("td")[1];
+	    td3 = tr[i].getElementsByTagName("td")[2];
+	    td4 = tr[i].getElementsByTagName("td")[3];
+	    if (td1) {
+	      txtValue1 = td1.textContent || td1.innerText;
+	      txtValue2 = td2.textContent || td2.innerText;
+	      txtValue3 = td3.textContent || td3.innerText;
+	      txtValue4 = td4.textContent || td4.innerText;
+	      if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1  || txtValue3.toUpperCase().indexOf(filter) > -1  || txtValue4.toUpperCase().indexOf(filter) > -1 ) {
+	        tr[i].style.display = "";
+	      } else {
+	        tr[i].style.display = "none";
+	      }
+	    }       
+	  }
 }
 function addRecord(obj,org){
-	var totalObj;
-	var count=localStorage.getItem(org+"_count");
+	let totalObj;
+	let count=localStorage.getItem(`${org}_count`);
 	if(count==0){
 		totalObj=JSON.stringify(obj);
 	}
 	else{
-		var getStringObj=localStorage.getItem(org+"_data");
-		var getObj = JSON.parse(getStringObj);
-		var totalObjString=getObj.concat(obj);
+		let getStringObj=localStorage.getItem(`${org}_data`);
+		let getObj = JSON.parse(getStringObj);
+		let totalObjString=getObj.concat(obj);
 		totalObj=JSON.stringify(totalObjString);
 	}
-	localStorage.setItem(org+"_data", totalObj);
+	localStorage.setItem(`${org}_data`, totalObj);
 	count++;
-	localStorage.setItem(org+"_count",count);
+	localStorage.setItem(`${org}_count`,count);
 	start();
 }
 function createStudent(){
-	var org=getOrg();
-	var obj=[{roll:document.getElementById(org+"_roll").value, name:document.getElementById(org+"_name").value, class:document.getElementById(org+"_class").value, advisor:document.getElementById(org+"_advisor").value}];
+	let org=getOrg();
+	let obj=[{roll:document.getElementById(`${org}_roll`).value, name:document.getElementById(`${org}_name`).value, class:document.getElementById(`${org}_class`).value, advisor:document.getElementById(`${org}_advisor`).value}];
 	addRecord(obj,org);
 }
 function studentTable(count,getObj){
-	var codeBlock="";
-	for(var i=0;i<count;i++){
-		codeBlock=codeBlock+"<tr><td>"+getObj[i].roll+"</td><td>"+getObj[i].name+"</td><td>"+getObj[i].class+"</td><td>"+getObj[i].advisor+"</td><td class='update_btns'><button class='btn btn-primary btn-md' onclick='getStu("+i+")'>Edit</button></td><td class='update_btns'><button class='btn btn-danger btn-md' onclick='deleteOrg("+i+")'>Delete</button></td></tr>";
+	let codeBlock="";
+	for(let i=0;i<count;i++){
+		codeBlock=codeBlock+`<tr><td>${getObj[i].roll}</td><td>${getObj[i].name}</td><td>${getObj[i].class}</td><td>${getObj[i].advisor}</td><td class='update_btns'><button class='btn btn-primary btn-md' onclick='getStu("${i}")'>Edit</button></td><td class='update_btns'><button class='btn btn-danger btn-md' onclick='deleteOrg("${i}")'>Delete</button></td></tr>`;
 	}
 	return codeBlock;
 }
 function putData(org){
-	var codeBlock="";
-	var count=localStorage.getItem(org+"_count");
-	var getStringObj=localStorage.getItem(`${org}_data`);
-	var getObj = JSON.parse(getStringObj);
+	let codeBlock="";
+	let count=localStorage.getItem(`${org}_count`);
+	let getStringObj=localStorage.getItem(`${org}_data`);
+	let getObj = JSON.parse(getStringObj);
 	if(org=="stu"){
 		codeBlock=studentTable(count,getObj);
 	}
 	else{
 		codeBlock=employeeTable(count,getObj);
 	}
-	document.getElementById(org+"_body").innerHTML = codeBlock;
+	document.getElementById(`${org}_body`).innerHTML = codeBlock;
 }
 function getOrg(){
-	var x = document.getElementById("org_id").value;
+	let x = document.getElementById("org_id").value;
 	return x;
 }
 function deleteOrg(index){
-	var org=getOrg();
-	var count=localStorage.getItem(org+"_count");
-	var getStringObj=localStorage.getItem(`${org}_data`);
-	var getObj = JSON.parse(getStringObj);
+	let org=getOrg();
+	let count=localStorage.getItem(`${org}_count`);
+	let getStringObj=localStorage.getItem(`${org}_data`);
+	let getObj = JSON.parse(getStringObj);
 	getObj.splice(index,1)
 	count--;
-	var totalObj=JSON.stringify(getObj);
-	localStorage.setItem(org+"_data", totalObj);
-	localStorage.setItem(org+"_count",count);
+	let totalObj=JSON.stringify(getObj);
+	localStorage.setItem(`${org}_data`, totalObj);
+	localStorage.setItem(`${org}_count`,count);
 	start();
 }
 function start(){
@@ -95,25 +95,25 @@ function start(){
 	document.getElementById("emp_section").style.display = "none";
 	document.getElementById("stu_table").style.display = "none";
 	document.getElementById("emp_table").style.display = "none";
-	var org=getOrg();
+	let org=getOrg();
 	selectChange(org);
-	if (localStorage.getItem(org+"_count") === null) {
-		localStorage.setItem(org+"_count", 0);
+	if (localStorage.getItem(`${org}_count`) === null) {
+		localStorage.setItem(`${org}_count`, 0);
 	}
-	var count=localStorage.getItem(org+"_count");
+	let count=localStorage.getItem(`${org}_count`);
 	putData(org);
-	var getStringObj=localStorage.getItem(org+"_data");
-	document.getElementById(org+"_table").style.display = "block";
+	let getStringObj=localStorage.getItem(`${org}_data`);
+	document.getElementById(`${org}_table`).style.display = "block";
 }
 function showAdd(){
-	var org=getOrg();
-	document.getElementById(org+"_table").style.display = "none";
-	document.getElementById(org+"_section").style.display = "block";
+	let org=getOrg();
+	document.getElementById(`${org}_table`).style.display = "none";
+	document.getElementById(`${org}_section`).style.display = "block";
 }
 function sortTable(n,num) {
-	var org=getOrg();
-  	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  	table = document.getElementById(org+"_atable");
+	let org=getOrg();
+  	let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  	table = document.getElementById(`${org}_atable`);
   	switching = true;
   	dir = "asc"; 
   	while (switching) {
